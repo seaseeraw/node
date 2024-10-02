bd34e3f5ca14822733785a9ffdeb380864f13e7f
seaseeraw <seaseeraw12@gmail.com>
2024-10-03 09:24:31 +1000

    some change

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
+import express from "express";
+import path from "path";
+import fs from "fs";
+import {home,login,register} from "./home.js";
 
-
-const express = require('express');
 const app = express();
+const PORT = 3000;
+const __dirname = path.resolve();
 
-app.get("/",(req,res)=>{
-  let htmlString ="<h1>Hello</h1>";
-  res.send(htmlString);
-  // res.sendFile(path,join("C:\Users\HP LAP\OneDrive\Desktop\DentedCode\Node class>" , "/file.json"));
-})
+app.use("/public", express.static(path.join(__dirname, "static")));
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  let htmlString = "<h1>HOME PAGE</h1>";
+  // response.send(htmlString);
 
-app.listen(3000,()=>{
-
+  res.sendFile(path.join(__dirname, "/file.json"));
 });
 
-// app.listen(3000,(error)=>{
-//   if(error){
-//     console.log("error occured",error );
-//   }
-//   else{
-//     console.log("is good");
-//   }
+// app.get("/", (req,res) => {
+//   let htmlString = "<h1>HOME PAGE</h1>";
+//   res.sendFile(path.join(__dirname, "/file.json"));
 // });
+
+app.get("/home",(req,res)=>{
+  let htmlString = home();
+  res.send(htmlString);
+});
+
+app.get("/login",(req,res)=>{
+  let htmlString = login();
+  console.log("LOGIN QUERY:", request.query);
+  res.send(htmlString);
+});
+
+app.get("/register", (req, res) => {
+  let htmlString = register();
+  res.send(htmlString);
+});
+
+// app.listen(3000,()=>{
+
+// });
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log("ERROR in serving")
+    : console.log("http://localhost:" + PORT + " started");
+});
